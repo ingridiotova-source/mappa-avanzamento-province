07fd0e2ba58ef746c111210d1fe3b3e17e70cd6b
ingridiotova-source <ingrid.iotova@assistfuture.it>
2026-05-29 14:46:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 1) Dopo aver pubblicato il foglio Google come CSV, incolla qui il link.
 // 2) Lascia vuoto per usare il CSV locale incluso nella cartella, utile solo per test.
 window.MAPPA_CONFIG = {
-  CSV_URL: ""
+  CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSj_ZsjdThu4JNbgK3onIjP9STGp10LNIdCSL2xhIEV4Xv-fOMU6_n9S7lUk-WbUA/pub?gid=370005405&single=true&output=csv"
 };
